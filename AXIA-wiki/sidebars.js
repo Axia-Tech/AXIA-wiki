0bb5b5e3d461bf71414b3d1a5d5fc3be780f194e
Arjun Chamyal <97728862+arjun-zeeve@users.noreply.github.com>
2022-02-16 21:59:55 +0530

    Update sidebars.js

--- AXIA-wiki/sidebars.js
+++ AXIA-wiki/sidebars.js
@@ -120,7 +120,7 @@ module.exports = {
             
             {
             type: "category",
-            label: "AXIA Wallet",
+            label: "AXwallet",
             items:[
             //TO DO
                 "build/build-storage",
@@ -132,7 +132,7 @@ module.exports = {
             
                {
                 type: "category",
-                label: "AXIA Explorer ",
+                label: "AXscan ",
                 items: [
  "build/build-oracle",
 //"build/build-node-management",
